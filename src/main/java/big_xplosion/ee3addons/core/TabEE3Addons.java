81b116b44d62ef214ccc61f0684de439c5505990
big_Xplosion <bigXplosion44@gmail.com>
2015-07-05 17:12:18 +0200

    Added creative tab

---
+++ src/main/java/big_xplosion/ee3addons/core/TabEE3Addons.java
@@ -0,0 +1,18 @@
+package big_xplosion.ee3addons.core;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class TabEE3Addons extends CreativeTabs {
+
+	public static TabEE3Addons tab = new TabEE3Addons();
+
+	public TabEE3Addons() {
+		super("ee3Addons");
+	}
+
+	@Override
+	public Item getTabIconItem() {
+		return null;
+	}
+}
