9857edc2979e95c8915ebdbf7bb56029fad5b903
big_Xplosion <bigXplosion44@gmail.com>
2015-11-29 22:30:35 +0100

    Preparing work for the portable transmutation tablet

--- src/main/java/big_xplosion/ee3addons/client/gui/GuiPortableTransmutationTablet.java
+++ src/main/java/big_xplosion/ee3addons/client/gui/GuiPortableTransmutationTablet.java
@@ -1,14 +1,12 @@
 package big_xplosion.ee3addons.client.gui;
 
-import big_xplosion.ee3addons.inventory.InventoryPortableTransmutationTablet;
-import big_xplosion.ee3addons.inventory.container.ContainerPortableTransmutationTablet;
 import com.pahimar.repackage.cofh.lib.gui.GuiBase;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.util.ResourceLocation;
 
-// Not using my own GuiBase as I'm lazy and can just copy code from EE3 this way.
 public class GuiPortableTransmutationTablet extends GuiBase {
 
-	public GuiPortableTransmutationTablet(EntityPlayer player, InventoryPortableTransmutationTablet tabletInv) {
-		super(new ContainerPortableTransmutationTablet(player, tabletInv));
+	public GuiPortableTransmutationTablet(Container container, ResourceLocation texture) {
+		super(container, texture);
 	}
 }
--- src/main/java/big_xplosion/ee3addons/inventory/container/ContainerPortableTransmutationTablet.java
+++ src/main/java/big_xplosion/ee3addons/inventory/container/ContainerPortableTransmutationTablet.java
@@ -1,6 +1,7 @@
 package big_xplosion.ee3addons.inventory.container;
 
 import big_xplosion.ee3addons.inventory.InventoryPortableTransmutationTablet;
+import com.pahimar.ee3.inventory.InventoryTransmutationTablet;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 
@@ -8,14 +9,25 @@ public class ContainerPortableTransmutationTablet extends Container {
 
 	private EntityPlayer player;
 	private InventoryPortableTransmutationTablet tabletInv;
+	private InventoryTransmutationTablet inventoryTransmutationTablet;
+
+	protected final int PLAYER_INVENTORY_ROWS = 3;
+	protected final int PLAYER_INVENTORY_COLUMNS = 9;
+
+	private float energyValue;
+	private String searchTerm;
+	private int sortOption;
+	private int sortOrder;
+	private int scrollBarPosition;
 
 	public ContainerPortableTransmutationTablet(EntityPlayer player, InventoryPortableTransmutationTablet tabletInv) {
 		this.player = player;
 		this.tabletInv = tabletInv;
 	}
 
+
 	@Override
-	public boolean canInteractWith(EntityPlayer player) {
-		return true;
+	public boolean canInteractWith(EntityPlayer p_75145_1_) {
+		return false;
 	}
 }
